b09e75b3107d389826cb046b27d0ea017ddd56a9
Vinod Soba <vinod_soba@hotmail.co.uk>
2026-08-02 13:58:57 +0000

    Refactor BeautyFort supplier sync

--- app/code/BeautyFort/BeautyfortProductImport/Cron/UpdatePrice.php
+++ app/code/BeautyFort/BeautyfortProductImport/Cron/UpdatePrice.php
@@ -15,8 +15,20 @@ class UpdatePrice
         $this->priceUpdater = $priceUpdater;
     }
 
-    public function execute(): void
+   public function execute(): void
     {
+         file_put_contents(
+            BP . '/var/log/updateprice.log',
+            "START " . date('Y-m-d H:i:s') . PHP_EOL,
+            FILE_APPEND
+        );
+
         $this->priceUpdater->execute();
+
+        file_put_contents(
+            BP . '/var/log/updateprice.log',
+            "END " . date('Y-m-d H:i:s') . PHP_EOL,
+            FILE_APPEND
+        );
     }
 }
